8f93b794756bed75af2da4c228e3b728445a836c
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-08-21 15:37:40 +0300

    JS

---
+++ Lesson-2/example/jscript.js
@@ -0,0 +1,11 @@
+function cart(addCart){
+
+    console.log('loading') = addCart;
+
+}
+
+function buy(purchase){
+
+    alert("Purchased!") = purchase;
+
+}
--- Lesson-2/jscript.js
+++
@@ -1,8 +0,0 @@
-var name = 'Arin';
-console.log(name); 
-
-var age = 3;
-console.log(age);
-
-var job = 'Doctor';
-console.log(job);
