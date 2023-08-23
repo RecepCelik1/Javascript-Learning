cd241f96a0746f86f9c40dec9b59c8adc52d9bdf
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-08-23 07:53:40 +0300

    Update variables.js

--- Lesson-3/lesson/variables.js
+++ Lesson-3/lesson/variables.js
@@ -17,7 +17,10 @@ console.log(); ifadesi bize öğeyi denetle menüsündeki konsol seçeneğine ge
 *console.log(variable1); konsolda, variable1 değişkeni içine depolanmış olan datayı çıkartır.
 
 
-    **Bu günki dersimiz değişkenler. Değişken ifadesinin ne olduğunu zaten biliyorum C dilinden fakat JS dilinde biraz farklılıkları var. C de atayacağımız değişken türleri vardır. Değişkenin içinde depolanacak olan dataya göre farklı farklı değişken türleri vardır. Fakat burada değişken türleri bulunmamaktadır yazdığımız datanın türü fark etmeksizin depolayacaktır. Hadi jsteki değişkenlerin syntaxını inceleyelim.
+    **Bu günki dersimiz değişkenler. Değişken ifadesinin ne olduğunu zaten biliyorum C dilinden fakat JS dilinde biraz farklılıkları 
+    var. C de atayacağımız değişken türleri vardır. Değişkenin içinde depolanacak olan dataya göre farklı farklı değişken türleri 
+    vardır. Fakat burada değişken türleri bulunmamaktadır yazdığımız datanın türü fark etmeksizin depolayacaktır. Hadi jsteki 
+    değişkenlerin syntaxını inceleyelim.
 
         * let variable1 = 4  yada let variable1 = 'string' 
 
@@ -25,6 +28,7 @@ console.log(); ifadesi bize öğeyi denetle menüsündeki konsol seçeneğine ge
 
             hemen sonrasında değişkene isim veririz. Her değişkenin ismi değişkene özel olmalıdır.
 
-            daha sonra = ile bir değer atarız o değişkene. Muhtemelen ileride son kullanıcıdan veri girişi almayı öğreneceğiz ve tıpkı C dilinde olduğu gibi kullanıcıdan alınan veriler değişkenlerin içinde depolanır.
+            daha sonra = ile bir değer atarız o değişkene. Muhtemelen ileride son kullanıcıdan veri girişi almayı öğreneceğiz ve tıpkı 
+            C dilinde olduğu gibi kullanıcıdan alınan veriler değişkenlerin içinde depolanır.
 
 */
