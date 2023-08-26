5650a199cc7cdc46e25e950771a1b047d240bd1f
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-08-26 12:35:46 +0300

    JS

---
+++ Lesson-4/booleans.js
@@ -0,0 +1,46 @@
+/*      Bu günki dersimiz booleanslar.Javascripte Şu ana kadar sadece iki çeşit veri tipi öğrendik. Sayılar ve metinler, booleans 
+javascripte başka bir veri türüdür.
+
+    **booleanslar özeldir sadece iki çeşit veri tipi alabilirler. true ve false. Adlarından da anlaşılabileceği üzere bir şeyin 
+        doğru mu yanlış olduğunu temsil eder. logic is loading :) .
+
+
+*/
+true 
+false
+
+console.log(3 > 5);   //aynı zamanda kıyaslama operatörüde boolean oluşturmanın bir yoludur pek tabi :)
+
+/*
+    **booleansların syntax dili
+
+        *booleans oluşturmak için true yada false yazarız kodun içine. booleanslar tırnak işaretleri ile sarılmaz yoksa o artık 
+        boolean değil bir string olur.
+
+*/
+
+console.log(typeof 'true');
+
+console.log(typeof true);
+
+/* bahsi geçmişken kıyaslama operatörleri (kıyaslama operatörleri aritmetik öperatörlerinden daha düşük öncelik sıralamaısına sahiptir.)
+    
+    *   > greater than
+    
+    *   < less than 
+    
+    *   >= greater than or equal to
+
+    *   <= lee than or equal to
+
+    *   === equal to
+
+    *   !== not equal to
+
+        ***artık booleans operatörlerini ve kıyaslama öperatörlerini öğrendik bunları if-else statements ile birlikte kullanacağız.***
+*/
+
+//      **C dilinden if yapısına hakimiz az çok. JS tete olaylar farklı değildir.
+
+
+
