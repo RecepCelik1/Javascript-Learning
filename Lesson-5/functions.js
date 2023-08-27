7b1a8192bc7aee141dc418d7bcadf7056f3bdd7f
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-08-27 12:43:24 +0300

    JS

---
+++ Lesson-5/functions.js
@@ -0,0 +1,20 @@
+/*      **Js fonksiyonlarını zaten kullanıyorduk şu ana kadar hadi daha detaylı tanımlandıralım.*/
+
+/*
+
+    function functionName() {       ==> yanda görüldüğü gibi kullanılır. süslü parantezler arasında kodlarımı yazarız.
+
+
+    }
+
+*/
+
+function function1(){
+    console.log('hello');
+    console.log(2 + 2);
+}
+
+/* fonksiyonu oluşturduktan sonra daha sonra gidip html dosyasında script etiketleri arasına fonksiyonun adını ve parametre listelerini
+yazarak fonksiyonumu çağırıp kullanabiliriz.
+ 
+ */
